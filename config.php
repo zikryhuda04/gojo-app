a13d75318ce816a1c1ef98f3a3aaf5e1551a424e
zikryhuda04 <153715823+zikryhuda04@users.noreply.github.com>
2025-05-24 15:25:14 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
-$koneksi = new mysqli("localhost", "root", "", "gojo_db");
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
+$servername = getenv('DB_HOST') ?: 'localhost';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
+$dbname = getenv('DB_NAME') ?: 'gojo_db';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
